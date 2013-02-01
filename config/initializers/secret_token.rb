62d455c3151f207ee195ac5de173581876bd3c83
Willy De la Cruz Sierra <wdelacruzsierra@gmail.com>
2013-02-01 06:58:35 -0500

    Base files and folders

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DEW2013Trabajofinal::Application.config.secret_token = '722268da9f5b0ece3d036b68ec6447555caae6ffbaa751ff0c57cb2474bb8782522b21c8a16428d296aed24ca286c2c158e9a74b13c66964a4a3562af77fb693'
