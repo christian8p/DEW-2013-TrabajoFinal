67e2a6cfe3445e2eed33c4e53032712804981219
Jose Rojo <el__red@hotmail.com>
2013-02-06 17:29:28 -0500

    integrando el login y logout localmente

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :id, :username
+
+  attr_accessible :crypted_password, :email, :password_salt, :persistence_token, :username, :password, :password_confirmation
+
 end
